ab2996b63cfa3308a644e801712ac89e5cd92078
vjtknn <wbwinnicki@gmail.com>
2017-01-17 20:50:52 +0100

    wyciąganie całości z ułamka

--- WindowsFormsApplication1/Ulamek.cs
+++ WindowsFormsApplication1/Ulamek.cs
@@ -16,19 +16,55 @@ namespace WindowsFormsApplication1
 
         public Ulamek(int licznik, int mianownik, int cale = 0)
         {
-            this.licznik = licznik + (cale * mianownik);
+            this.licznik = licznik;
             this.mianownik = mianownik;
-
+            this.cale = cale;
         }
 
         public Ulamek Dodaj(Ulamek x)
         {
+            licznik = licznik + (cale * mianownik);
+            x.licznik = x.licznik + (x.cale * x.mianownik);
+
             int f_licznik = (licznik * x.mianownik) + (x.licznik * mianownik);
             int f_mianownik = mianownik * x.mianownik;
             int f_cale = 0;
 
-        
-            int mniejsze = coMiększe(f_mianownik, f_mianownik);
+            if (f_licznik > f_mianownik)
+            {
+                f_cale = f_licznik / f_mianownik;
+                f_licznik = f_licznik % f_mianownik;
+            }
+
+
+            int mniejsze = coMiększe(f_licznik, f_mianownik);
+
+            for(int i = 2; i < mniejsze; i++)
+            {
+                if(f_mianownik % i == 0 && f_licznik % i == 0)
+                {
+                    f_mianownik = f_mianownik / i;
+                    f_licznik = f_licznik / i;
+                    break;
+                }
+            }
+            return new Ulamek(f_licznik, f_mianownik, f_cale);
+        }
+         public Ulamek Odejmij(Ulamek x)
+        {
+            licznik = licznik + (cale * mianownik);
+            x.licznik = x.licznik + (x.cale * x.mianownik);
+            int f_licznik = (this.licznik * x.mianownik) - (x.licznik * this.mianownik) ;
+            int f_mianownik = this.mianownik * x.mianownik;
+
+            int f_cale = 0;
+
+            if (f_licznik > f_mianownik)
+            {
+                f_cale = f_licznik / f_mianownik;
+                f_licznik = f_licznik % f_mianownik;
+            }
+            int mniejsze = coMiększe(f_licznik, f_mianownik);
 
             for (int i = 2; i < mniejsze; i++)
             {
@@ -39,37 +75,64 @@ namespace WindowsFormsApplication1
                     break;
                 }
             }
+            return new Ulamek(f_licznik, f_mianownik, f_cale);
+        }
+        public Ulamek Pomnoz(Ulamek x)
+        {
+            licznik = licznik + (cale * mianownik);
+            x.licznik = x.licznik + (x.cale * x.mianownik);
+            int f_licznik = this.licznik * x.licznik;
+            int f_mianownik = this.mianownik * x.mianownik;
+            int f_cale = 0;
 
             if (f_licznik > f_mianownik)
             {
                 f_cale = f_licznik / f_mianownik;
                 f_licznik = f_licznik % f_mianownik;
             }
-         
+
+
+            int mniejsze = coMiększe(f_licznik, f_mianownik);
+
+            for (int i = 2; i < mniejsze; i++)
+            {
+                if (f_mianownik % i == 0 && f_licznik % i == 0)
+                {
+                    f_mianownik = f_mianownik / i;
+                    f_licznik = f_licznik / i;
+                    break;
+                }
+            }
             return new Ulamek(f_licznik, f_mianownik, f_cale);
-        }
-         public Ulamek Odejmij(Ulamek x)
-        {
-            int f_licznik = (this.licznik * x.mianownik) - (x.licznik * this.mianownik) ;
-            int f_mianownik = this.mianownik * x.mianownik;
-
-            return new Ulamek(f_licznik, f_mianownik);
-        }
-        public Ulamek Pomnoz(Ulamek x)
-        {
-            int f_licznik = this.licznik * x.licznik;
-            int f_mianownik = this.mianownik * x.mianownik;
-
-            return new Ulamek(f_licznik, f_mianownik);
         }
         public Ulamek Podziel(Ulamek x)
         {
+            licznik = licznik + (cale * mianownik);
+            x.licznik = x.licznik + (x.cale * x.mianownik);
             int f_licznik = this.licznik * x.mianownik;
             int f_mianownik = this.mianownik * x.licznik;
 
-            return new Ulamek(f_licznik, f_mianownik);
-        }
+            int f_cale = 0;
 
+            if (f_licznik > f_mianownik)
+            {
+                f_cale = f_licznik / f_mianownik;
+                f_licznik = f_licznik % f_mianownik;
+            }
+
+            int mniejsze = coMiększe(f_licznik, f_mianownik);
+
+            for (int i = 2; i < mniejsze; i++)
+            {
+                if (f_mianownik % i == 0 && f_licznik % i == 0)
+                {
+                    f_mianownik = f_mianownik / i;
+                    f_licznik = f_licznik / i;
+                    break;
+                }
+            }
+            return new Ulamek(f_licznik, f_mianownik, f_cale);
+        }
         public int coMiększe(int a, int b)
         {
             if (a > b)
@@ -84,9 +147,6 @@ namespace WindowsFormsApplication1
             {
                 return b;
             }
-
         }
-
-        
     }
 }
